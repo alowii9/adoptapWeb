be351a904ad46151ca41463aff1e36abd32b5206
Maximiliano Ariel Torres <69557158+alowii9@users.noreply.github.com>
2024-02-29 12:45:32 -0300

    BUGFIX: Minor code refactoring.

--- app/registrarse/page.tsx
+++ app/registrarse/page.tsx
@@ -1,77 +1,13 @@
-'use client'
-
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import firebase from "firebase/compat/app";
 import db from "@/app/Firebase/FirebaseConfig";
 import Swal from "sweetalert2";
 import $ from 'jquery';
-import { BrowserRouter, Navigate, BrowserRouter as Router, useNavigate ,   } from 'react-router-dom';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-
+import { BrowserRouter, Navigate, BrowserRouter as Router, useNavigate } from 'react-router-dom';
 
 const Registrarse = () => {
- 
-
-  async function registrarUsuario(usuario: { name: any; email: any; password: any; }) {
-    const { name, email, password} = usuario;
-    const auth = getAuth();
-    try {
-      const infoUsuario = await createUserWithEmailAndPassword(
-        auth, email, password
-      );
-      crearUsuario({ name, email, password });
-    } catch (error) {
-      
-      MSJERROR();
-    }
-  }
-
-
-  //funcion para agregar publicacion en firebase
-const mover = useNavigate();
-
-const crearUsuario = (usuario: any) => {
-  const { name, email, password} = usuario;
-    db.collection("usuarios").add({
-        name,
-        email,
-        password
-    })
-    .then(function(docRef) {
-        MSJOK();
-        mover("/login")
- })
-    .catch(function(error)
-    {  
-       MSJERROR();
-    })
-}
-
-//mensaje de OK
-const MSJOK = () => {
-    Swal.fire({
-        title: "Buen Trabajo!",
-        text: "Publicacion creada!",
-        icon: "success"
-      });
-      
-
-}
-// mensaje de error
-const MSJERROR = () => {
-    Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "publicacion no creada, verifica los campos!!",
-      });
-      
-}
-
-
-
-
-useEffect(() => {
-  async function registrarUsuario(usuario) {
+  const [registrarUsuario, setRegistrarUsuario] = useState(() => async (usuario) => {
     const { name, email, password } = usuario;
     const auth = getAuth();
     try {
@@ -80,100 +16,112 @@ useEffect(() => {
     } catch (error) {
       MSJERROR();
     }
-  }
+  });
 
-  const handleClick = () => {
-    let name = $("#name").val();
-    let email = $("#email").val();
-    let password = $("#password").val();
+  const crearUsuario = useCallback((usuario) => {
+    const { name, email, password } = usuario;
+    db.collection("usuarios").add({
+      name,
+      email,
+      password
+    })
+      .then(function (docRef) {
+        MSJOK();
+        mover("/login")
+      })
+      .catch(function (error) {
+        MSJERROR();
+      })
+  }, [MSJOK, MSJERROR, mover]);
 
-    const usuario = {
-      name, email, password
+  const MSJERROR = useCallback(() => {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "publicacion no creada, verifica los campos!!",
+    });
+  }, []);
+
+  const MSJOK = useCallback(() => {
+    Swal.fire({
+      title: "Buen Trabajo!",
+      text: "Publicacion creada!",
+      icon: "success"
+    });
+  }, []);
+
+  const mover = useNavigate();
+
+  useEffect(() => {
+    const handleClick = () => {
+      let name = $("#name").val();
+      let email = $("#email").val();
+      let password = $("#password").val();
+
+      const usuario = {
+        name, email, password
+      }
+
+      if (!name || !email || !password) {
+        MSJERROR();
+      } else {
+        registrarUsuario(usuario);
+      }
     }
 
-    if (!name || !email || !password) {
-      MSJERROR();
-    } else {
-      registrarUsuario(usuario);
-    }
-  }
+    $("#btnsave").on('click', handleClick);
 
-  $("#btnsave").on('click', handleClick);
+    return () => {
+      $("#btnsave").off('click', handleClick);
+    };
+  }, [registrarUsuario, MSJERROR]);
 
-  return () => {
-    $("#btnsave").off('click', handleClick);
-  };
-}, [crearUsuario, MSJERROR]);
-
-
-return (
-  
-   <>
-
-<div style={fondoColorPagina}>
-
-<h1 style={h1}>Crear Nuevo Usuario</h1>
-  
-  <form style={formStyle}>
-    
-      <div>
-        <label htmlFor="nombre">Nombre:</label><br />
-        <input style={inputStyle}
-          type="text"
-          id="name"
-          placeholder="Ingrese su nombre"
-          
-        />
+  return (
+    <>
+      <div style={fondoColorPagina}>
+        <h1 style={h1}>Crear Nuevo Usuario</h1>
+        <form style={formStyle}>
+          <div>
+            <label htmlFor="nombre">Nombre:</label><br />
+            <input style={inputStyle}
+              type="text"
+              id="name"
+              placeholder="Ingrese su nombre"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">email:</label><br />
+            <input style={inputStyle}
+              type="text"
+              id="email"
+              placeholder="Ingrese su correo"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">password:</label><br />
+            <input style={inputStyle}
+              type="password"
+              id="password"
+              placeholder="Ingrese su contrasenia"
+            />
+          </div>
+          <button id="btnsave" style={buttonStyle} type="button">Crear usuario</button>
+        </form>
+        <div  style={volverInicio}>
+          <button ><a  href="/">volver al inicio</a></button>
+        </div>
       </div>
-
-      <div>
-        <label htmlFor="email">email:</label><br />
-        <input style={inputStyle}
-          type="text"
-          id="email"
-          placeholder="Ingrese su correo"
-          
-        />
-      </div>
-
-      <div>
-        <label htmlFor="password">password:</label><br />
-        <input style={inputStyle}
-          type="password"
-          id="password"
-          placeholder="Ingrese su contrasenia"
-          
-        />
-      </div>
-       
-      
-      <button id="btnsave" onClick={() => <Navigate to={'/principal'} />}  style={buttonStyle} type="button">Crear usuario</button>
-    
-    </form>
-
-  <div  style={volverInicio}>
-  <button ><a  href="/">volver al inicio</a></button>
-  </div>
-
-</div>  
-</>
-
-)
-
+    </>
+  );
 }
 
-
-
 //Estilos formulario
-
 const fondoColorPagina = {
   background: "#340434",
   color: "#fff",
-
 }
 
 const h1 = {
- 
   paddingBottom: "50px",
   paddingTop: "100px"
 }
@@ -206,14 +154,13 @@ const buttonStyle = {
 };
 
 // estilo boton volver
-
 const volverInicio = {
   display:"flex",
   justifyContent: "center",
   alignItems: "center",
- height :"10vh",
- paddingTop: "20px",
- marginTop: "100px",
+  height :"10vh",
+  paddingTop: "20px",
+  marginTop: "100px",
 }
 
 export default Registrarse;
